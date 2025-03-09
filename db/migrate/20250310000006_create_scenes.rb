ff009b254665c540c9010eab8a7cb0d9618caf3d
Owen Baldwin <90186455+owenbaldwin@users.noreply.github.com>
2025-03-09 15:29:39 +0000

    Add Scene migration

---
+++ db/migrate/20250310000006_create_scenes.rb
@@ -0,0 +1,15 @@
+class CreateScenes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :scenes do |t|
+      t.references :sequence, null: false, foreign_key: true
+      t.integer :number, null: false
+      t.string :name
+      t.text :description
+      t.string :setting
+      t.string :time_of_day
+
+      t.timestamps
+    end
+    add_index :scenes, [:sequence_id, :number], unique: true
+  end
+end
