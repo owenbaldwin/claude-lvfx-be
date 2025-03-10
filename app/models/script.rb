f960d58485fc94f3349cd0cab0d8f54b1f75bc4f
Owen Baldwin <90186455+owenbaldwin@users.noreply.github.com>
2025-03-10 14:42:30 +0000

    Update Script model with nested associations

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,6 +1,9 @@
 class Script < ApplicationRecord
   belongs_to :production
   has_many :sequences, dependent: :destroy
+  has_many :scenes, through: :sequences
+  has_many :action_beats, through: :scenes
+  has_many :shots, through: :action_beats
   
   validates :title, presence: true
 end
